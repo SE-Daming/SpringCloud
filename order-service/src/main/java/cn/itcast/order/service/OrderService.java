37e1335645742fe67bcae119f90cb247830873a7
SE Daming <930673083@qq.com>
2024-03-24 13:17:49 +0800

    restTemplate远程调用、但页面一直在加载，发送了很多次请求

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -2,18 +2,26 @@ package cn.itcast.order.service;
 
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
+import cn.itcast.order.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
 
     @Autowired
     private OrderMapper orderMapper;
+    @Autowired
+    RestTemplate restTemplate;
 
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
+        // 2.查询用户
+        String url="http://127.0.0.1:8080/order/"+order.getId();
+        User user  = restTemplate.getForObject(url, User.class);
+        order.setUser(user);
         // 4.返回
         return order;
     }
